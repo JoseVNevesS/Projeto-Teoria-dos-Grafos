11c4701d247d618fc22a61c45f31ac4c8812fe9f
JoseVNevesS <josevneves@outlook.com>
2022-05-05 13:17:36 -0300

    Matriz

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from numpy import matrix
+
+
 arq = open("grafo.txt")
 
 # Funções
@@ -36,6 +39,18 @@ def maiorGrau(vertices):
         if len(vertice) >= maior:
             maior = len(vertice)
     return(maior)
+
+def criarMatriz(dimensao, vertices):
+    matriz = []
+    for i in range(dimensao):
+        linha = []
+        for j in range(dimensao):
+            linha.append(0)
+            for vertice in range(int(len(vertices[i]))):       
+                if j+1 == int(vertices[i][vertice]):
+                    linha[j] = 1
+        matriz.append(linha)
+    return(matriz)
 ###############################################################################
 # Código
 
@@ -47,13 +62,22 @@ arestas = lerArestas()
 for aresta in arestas:
     for i in range(len(vertices)):
         if int(aresta[0]) == i+1:
-            vertices[i].append(aresta)
+            vertices[i].append(aresta[1])
+        if int(aresta[1]) == i+1:
+            vertices[i].append(aresta[0])
         
-
-
-print("Arestas:")
-print(arestas)
 print("Quantidade de vértices: " + str(len(vertices)))
 print("Quantidade de Arestas: " + str(len(arestas)))
 print("Menor Grau: " + str(menorGrau(vertices)))
 print("Maior Grau: " + str(maiorGrau(vertices)))
+
+matrizAdj = criarMatriz(len(vertices), vertices)
+print("Matriz de Adjacência: ")
+
+string = "   "
+for i in range(len(vertices)):
+    string += str(i+1) + "  "
+print(string)
+
+for i in range(len(vertices)):
+    print(str(i+1) + " " + str(matrizAdj[i]))
